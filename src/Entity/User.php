1cc13f035544a6135f3a77380038adf587cc6813
JD297 <jan.dommasch297@gmail.com>
2021-05-24 14:14:53 +0100

    Added __toString to UserEntity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -110,4 +110,9 @@ class User implements UserInterface
     {
         $this->plainPassword = null;
     }
+
+    public function __toString(): string
+    {
+	    return $this->getUsername();
+    }
 }
