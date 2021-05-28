e0bebba71c2b3edd3f9635099437b8117dfbb84b
JD297 <jan.dommasch297@gmail.com>
2021-05-28 22:23:06 +0100

    Removed request property from Event

--- src/Framework/Event/Event.php
+++ src/Framework/Event/Event.php
@@ -2,30 +2,6 @@
 
 namespace App\Framework\Event;
 
-use Symfony\Component\HttpFoundation\Request;
-
 class Event extends \Symfony\Contracts\EventDispatcher\Event
 {
-	protected Request $request;
-
-	public function __construct(Request $request)
-	{
-		$this->request = $request;
-	}
-
-	/**
-	 * @return Request
-	 */
-	public function getRequest(): Request
-	{
-		return $this->request;
-	}
-
-	/**
-	 * @param Request $request
-	 */
-	public function setRequest(Request $request): void
-	{
-		$this->request = $request;
-	}
 }
