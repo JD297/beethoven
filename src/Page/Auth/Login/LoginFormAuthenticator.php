9279d386b6d96c1c6d2f45a09c044a96792c28be
JD297 <jan.dommasch297@gmail.com>
2021-05-30 17:34:45 +0100

    Fixed redirect route to home

--- src/Page/Auth/Login/LoginFormAuthenticator.php
+++ src/Page/Auth/Login/LoginFormAuthenticator.php
@@ -102,7 +102,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-		return new RedirectResponse($this->urlGenerator->generate('frontend.forum.index.page'));
+		return new RedirectResponse($this->urlGenerator->generate('frontend.home.index.page'));
     }
 
     protected function getLoginUrl()
