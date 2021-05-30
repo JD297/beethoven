da3239509f6ed9ab7398b32e1c1e957d0e0a1749
JD297 <jan.dommasch297@gmail.com>
2021-05-30 21:43:13 +0100

    Removed generated code

--- src/Migration/Version20210530182522.php
+++ src/Migration/Version20210530182522.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20210530182522 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,14 +16,12 @@ final class Version20210530182522 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment CHANGE content content MEDIUMTEXT NOT NULL');
         $this->addSql('ALTER TABLE post ADD active TINYINT(1) NOT NULL, CHANGE content content MEDIUMTEXT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment CHANGE content content MEDIUMTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE post DROP active, CHANGE content content MEDIUMTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
