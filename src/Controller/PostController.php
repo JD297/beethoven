b117d129ba33bcb3848b5f78a000008be7565d98
JD297 <jan.dommasch297@gmail.com>
2021-05-31 20:44:55 +0100

    Added addComment function on PostPage

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Page\Post\CommentFormType;
 use App\Page\Post\PostPageLoader;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,8 +30,47 @@ class PostController extends AbstractController
 	{
 		$page = $this->postPageLoader->load($postId);
 
+		$form = $this->createForm(CommentFormType::class);
+
 		return $this->render('page/post/index.html.twig', [
-			'page' => $page->getData()
+			'page' => $page->getData(),
+			'commentForm' => $form->createView(),
+		]);
+	}
+
+	/**
+	 * @Route("/post/{postId}", name="frontend.post.add.comment", requirements={"postId"="\d+"}, methods={"POST"})
+	 * @ParamConverter("post", options={"id" = "postId"})
+	 * @param Post $post
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function addComment(Post $post, Request $request): Response
+	{
+		$comment = new Comment();
+
+		$form = $this->createForm(CommentFormType::class);
+		$form->handleRequest($request);
+
+		/** @var User $user */
+		$user = $this->getUser();
+
+		if ($form->isSubmitted() && $form->isValid() && $user) {
+			$comment
+				->setContent($form->get('content')->getData())
+				->setUser($user)
+				->setPost($post)
+			;
+
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($comment);
+			$entityManager->flush();
+		} else {
+			$this->addFlash('error', 'Sorry, this comment can not be added.');
+		}
+
+		return $this->redirectToRoute('frontend.post.index.page', [
+			'postId' => $post->getId(),
 		]);
 	}
 }
---
+++ src/Page/Post/CommentFormType.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Page\Post;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CommentFormType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('content', TextareaType::class, [
+				'constraints' => [
+					new NotBlank([
+						'message' => 'Comment should to be empty'
+					])
+				]
+			])
+			->add('submit', SubmitType::class)
+		;
+	}
+
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => Comment::class,
+		]);
+	}
+}
