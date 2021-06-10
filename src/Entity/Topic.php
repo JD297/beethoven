617eb63afb418867189aa31642270b2b39bd6c10
JD297 <jan.dommasch297@gmail.com>
2021-06-10 20:37:01 +0100

    Added toString method to TopicEntity

--- src/Entity/Topic.php
+++ src/Entity/Topic.php
@@ -55,7 +55,12 @@ class Topic
         $this->posts = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    public function __toString(): string
+    {
+	    return $this->getName();
+    }
+
+	public function getId(): ?int
     {
         return $this->id;
     }
