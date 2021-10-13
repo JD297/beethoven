21d49acefe19d18bf146c6ca3b4ed78d9d956b2f
JD297 <jan.dommasch297@gmail.com>
2021-10-13 19:41:30 +0200

    Fixed PostControllerTest

--- src/Test/Controller/Frontend/PostControllerTest.php
+++ src/Test/Controller/Frontend/PostControllerTest.php
@@ -2,15 +2,19 @@
 
 namespace Beethoven\Test\Controller\Frontend;
 
+use Beethoven\Entity\Post;
+use Beethoven\Entity\Topic;
+use Beethoven\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 class PostControllerTest extends WebTestCase
 {
-	public function testTopicResponseOK(): void
+	public function testPostResponseOK(): void
 	{
 		$client = static::createClient();
 		$client->catchExceptions(true);
@@ -21,14 +25,34 @@ class PostControllerTest extends WebTestCase
 		/** @var RouterInterface $router */
 		$router = $this->getContainer()->get('router');
 
+		/** @var UserPasswordHasherInterface $passwordHasher */
+		$passwordHasher = $this->getContainer()->get('security.user_password_hasher');
+
+		$user = new User();
+		$user
+			->setUsername('demo')
+			->setPassword(
+				$passwordHasher->hashPassword(
+					$user,
+					'demo'
+		));
+
+		$topic = new Topic();
+		$topic->setName('Test Topic');
+
 		$post = new Post();
 		$post->setName('Test Post');
+		$post->setContent('Some content for test post.');
+		$post->setUser($user);
+		$post->setTopic($topic);
 
+		$em->persist($user);
+		$em->persist($topic);
 		$em->persist($post);
 		$em->flush();
 
-		$client->request(Request::METHOD_GET, $router->generate('frontend.topic.index.page', [
-			'topicId' => $post->getId(),
+		$client->request(Request::METHOD_GET, $router->generate('frontend.post.index.page', [
+			'postId' => $post->getId(),
 		]));
 
 		$this->assertResponseStatusCodeSame(Response::HTTP_OK);
